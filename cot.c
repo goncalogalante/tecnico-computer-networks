1f0b7281afaeb55708f026567a20806520cbb2b0
Gonçalo Galante <72885029+goncalogalante@users.noreply.github.com>
2023-03-13 18:38:02 +0000

    create name, delete name, exit working

--- cot.c
+++ cot.c
@@ -11,24 +11,103 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-// Me node structure
-struct me{
+// structures:
 
-	int id;
+typedef struct {
+    char name[100];
+    
+} Conteudo;
+
+// Me node structure
+typedef struct me{
+
     char *ip;
     int port;
 
     char *ip_nodeserver;
     int port_nodeserver;
-};
+
+    int id; // my id
+    int ext; // extern neighborhood id
+    int bck; // backup neighborhood id
+
+    int my_net; // net nº where I belong
+
+
+    Conteudo conteudos[100]; // array de conteúdos
+    int num_conteudos; // actual nº of conteudos in the list
+
+} No;
+
+
+// auxiliar functions
+
+// function to print the command menu
+void print_menu() {
+
+  printf("\n--------------------Command Menu-------------------\n");  
+  printf("1) join net id: Entrada de um nó na rede net com identificador id.\n");
+  printf("2) djoin net id: bootid bootIP bootTCP Entrada de um nó na rede net com identificador id,\n");
+  printf("   que se sabe ser único na rede. É passado à aplicação o identificador e o contacto de um nó da rede,\n");
+  printf("   através dos parâmetros bootid, bootIP e bootTCP, ao qual o nó se deverá ligar sem interrogar o servidor de nós.\n");
+  printf("3) create name: É criado um conteúdo de nome name.\n");
+  printf("4) delete name: É apagado o conteúdo de nome name.\n");
+  printf("5) get dest name: Pesquisa do conteúdo com o nome name localizado no nó dest.\n");
+  printf("6) show topology (st): Mostra os identificadores e os contactos dos vizinhos internos, do vizinho externo e do vizinho de recuperação.\n");
+  printf("7) show names (sn): Mostra os nomes dos conteúdos presentes no nó.\n");
+  printf("8) show routing (sr): Mostra a tabela de expedição do nó.\n");
+  printf("9) leave: Saída do nó da rede.\n");
+  printf("10) exit: Fecho da aplicação.\n");
+}
+
+// function to create a content name for the node 
+void create(char *nome_conteudo, No *no)
+{
+
+Conteudo new_conteudo;
+strcpy(new_conteudo.name, nome_conteudo); 
+no->conteudos[no->num_conteudos] = new_conteudo;
+no->num_conteudos++;
+printf("Contents of the node:\n");
+    for (int i = 0; i < no->num_conteudos; i++) {
+        printf("%s \n", no->conteudos[i].name);
+    }
+}
+
+// function to delete a content name for the node
+void delete(char *nome_conteudo, No *no)
+{
+    int i, j;
+    for (i = 0; i < no->num_conteudos; i++) {
+        if (strcmp(no->conteudos[i].name, nome_conteudo) == 0) { // find the content to be deleted
+            
+            for (j = i; j < no->num_conteudos-1; j++) {
+                no->conteudos[j] = no->conteudos[j+1];
+            }
+            no->num_conteudos--; 
+            printf("Content removed.\n");
+        }
+    }
+    printf(" >> Error: Content not found.\n");
+
+    printf("Contents of the node:\n");
+    for (int i = 0; i < no->num_conteudos; i++) {
+        printf("%s \n", no->conteudos[i].name);
+    }
+}
+
+
 
 typedef struct me me_node;
 me_node me_ptr;
 
-
 int main(int argc, char *argv[]) {
 
+    // define the structure inside main
     struct me me_ptr;
+
+    // create a user input variable
+    char input[500];
 
     // verify the number of arguments
     if (argc != 5) {
@@ -80,13 +159,80 @@ int main(int argc, char *argv[]) {
     printf("- Server of Nodes UDP Port: %d\n", me_ptr.port_nodeserver);
 
 
+    // print the menu
+    print_menu();
+    while (1) {
+        printf("\n>> Command input: ");
+        fgets(input, 100, stdin);
+        input[strcspn(input, "\n")] = 0; // Remove the newline character
+
+        char *first_word = strtok(input, " ");
+
+        if (strcmp(first_word, "join") == 0) {
+            printf("Command selected: %s\n", first_word);
+        }
+
+        if (strcmp(first_word, "djoin") == 0) {
+            printf("Command selected: %s\n", first_word);
+        }
+
+        if (strcmp(first_word, "create") == 0) {
+            printf("Command selected: %s\n", first_word);
+
+            // takes the content name and saves it to the structure
+            char *content_name = strtok(NULL," ");
+            if (content_name == NULL) { 
+                printf(">> Error: Not a valid content.\n");
+            } else {
+            No no;
+            create(content_name, &no); // passa o nome do conteúdo e o ponteiro para a estrutura No para a função create()
+            }
+
+        }
+
+        if (strcmp(first_word, "delete") == 0) {
+            printf("Command selected: %s\n", first_word);
+            // takes the content name and saves it to the structure
+            char *content_name = strtok(NULL," ");
+            if (content_name == NULL) { 
+                printf(">> Error: Not a valid content.\n");
+            }
+            else {
+            No no;
+            delete(content_name, &no); // passa o nome do conteúdo e o ponteiro para a estrutura No para a função create()
+            }
+        }
+
+        if (strcmp(first_word, "get") == 0) {
+            printf("Command selected: %s\n", first_word);
+        }
+
+        if (strcmp(first_word, "show") == 0) {
+            printf("Command selected: %s\n", first_word);
+        }
+
+        if (strcmp(first_word, "sn") == 0) {
+            printf("Command selected: %s\n", first_word);
+        }
+
+        if (strcmp(first_word, "sr") == 0) {
+            printf("Command selected: %s\n", first_word);
+        }
+
+        if (strcmp(first_word, "leave") == 0) {
+            printf("Command selected: %s\n", first_word);
+        }
+
+        if (strcmp(first_word, "exit") == 0) {
+            printf("Command selected: %s\n", first_word);
+            printf("Exiting the program...");
+            break;
+        }
+
+        print_menu();
 
 
-
-
-
-
-
+    }
 
 
     }
